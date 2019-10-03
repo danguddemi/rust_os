f257e550a197736ac231d9f166377da2c95354c0
Daniel Guddemi <danguddemi@gmail.com>
2019-10-03 14:26:09 -0700

    Minimal Rust Kernel

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,19 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello, World!";
+
 #[no_mangle] // Compile the name exactly so that the linker can find the default `_start`
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
